c118ded83abc9b0b4e18e7f9522e88e93ec8c4b5
Damian Gryski <damian@gryski.com>
2020-08-24 07:04:03 -0700

    more benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -138,15 +138,35 @@ mod tests {
         );
     }
 
-    #[bench]
-    fn benchmark_hash(b: &mut Bencher) {
-        let mut val: u32 = 0;
+    macro_rules! bench_hash {
+        ($name:ident, $size:expr) => {
+            #[bench]
+            fn $name(b: &mut Bencher) {
+                let mut val: u32 = 0;
 
-        let mut v = Vec::<u8>::new();
-        v.resize(64, 0);
+                let mut v = Vec::<u8>::new();
+                v.resize($size, 0);
 
-        b.iter(|| {
-            val += hash(0, &v);
-        })
+                b.iter(|| {
+                    val += hash(0, &v);
+                })
+            }
+        };
     }
+
+    bench_hash!(benchmark_0008, 8);
+    bench_hash!(benchmark_0016, 16);
+    bench_hash!(benchmark_0032, 32);
+    bench_hash!(benchmark_0040, 40);
+    bench_hash!(benchmark_0060, 60);
+    bench_hash!(benchmark_0064, 64);
+    bench_hash!(benchmark_0072, 72);
+    bench_hash!(benchmark_0080, 80);
+    bench_hash!(benchmark_0100, 100);
+    bench_hash!(benchmark_0150, 150);
+    bench_hash!(benchmark_0200, 200);
+    bench_hash!(benchmark_0250, 250);
+    bench_hash!(benchmark_0512, 512);
+    bench_hash!(benchmark_1024, 1024);
+    bench_hash!(benchmark_8192, 8192);
 }
